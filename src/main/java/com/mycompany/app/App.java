40c73391be843c2f073732ad04d3131aeb7e873d
Tigran Najaryan <tnajaryan@splunk.com>
2023-01-09 09:22:15 -0500

    Enable trace context logging

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,8 +1,11 @@
 package com.mycompany.app;
 
+//import java.util.UUID;
+
 import org.apache.logging.log4j.LogManager;
 
-import io.opentelemetry.api.OpenTelemetry;
+//import io.opentelemetry.api.OpenTelemetry;
+//import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
@@ -11,12 +14,14 @@ import io.opentelemetry.context.Scope;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import io.opentelemetry.api.logs.GlobalLoggerProvider;
 import io.opentelemetry.api.logs.Logger;
@@ -29,16 +34,28 @@ import io.opentelemetry.sdk.logs.SdkLoggerProvider;
  */
 public class App {
 
-    public static OpenTelemetry openTelemetry;
+    public static OpenTelemetrySdk openTelemetry;
     
     private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
         logger.info("App is starting");
         initOtel();
-        createLog();
         createSpan();
+        createRawLogRecord();
     }    
+
+    // public static void initOtelAuto() {
+    //     AutoConfiguredOpenTelemetrySdk.builder()
+    //     .addResourceCustomizer(
+    //         (resource, configProperties) ->
+    //             resource.merge(
+    //                 Resource.builder()
+    //                     .put("service.instance.id", UUID.randomUUID().toString())
+    //                     .build()))
+    //     .build();
+
+    // }
 
     public static void initOtel() 
     {
@@ -47,6 +64,7 @@ public class App {
 
         SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
             .addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().build()).build())
+            .addSpanProcessor(SimpleSpanProcessor.create(LoggingSpanExporter.create()))
             .setResource(resource)
             .build();
 
@@ -55,18 +73,22 @@ public class App {
             .setResource(resource)
             .build();
 
-        openTelemetry = OpenTelemetrySdk.builder()
-            .setTracerProvider(sdkTracerProvider)
-            .setMeterProvider(sdkMeterProvider)
-            .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-            .buildAndRegisterGlobal();
-
         SdkLoggerProvider sdkLoggerProvider =
             SdkLoggerProvider.builder()
               .setResource(resource)
               //.addLogProcessor(...)
               .build();
-          GlobalLoggerProvider.set(sdkLoggerProvider);
+
+        openTelemetry = OpenTelemetrySdk.builder()
+            .setTracerProvider(sdkTracerProvider)
+            .setMeterProvider(sdkMeterProvider)
+            .setLoggerProvider(sdkLoggerProvider)
+            .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+            .buildAndRegisterGlobal();
+
+        GlobalLoggerProvider.set(sdkLoggerProvider);
+
+        //GlobalLoggerProvider.set(openTelemetry.getSdkLoggerProvider());
 
         logger.info("Otel is initialized");
     }
@@ -83,7 +105,7 @@ public class App {
         }
     }
 
-    public static void createLog() {
+    public static void createRawLogRecord() {
         LoggerProvider provider = GlobalLoggerProvider.get();
 
         Logger logger = provider.get("instrumentation-library-name");
